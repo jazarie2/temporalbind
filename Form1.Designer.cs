bc1b9f5acdac7a6d80044cbd2b4ca2068adb45ce
Abdul Aziz Abdul Muttalib <abdul_aziz@jabil.com>
2020-04-06 12:00:01 +0800

    First Draft Temporal Bind

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TemporalBind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog4 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(713, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(613, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(631, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "File2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(12, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(613, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(631, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "File3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(12, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(613, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(631, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "File4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog1";
+            // 
+            // openFileDialog4
+            // 
+            this.openFileDialog4.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 120);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(775, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(723, 92);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(20, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(750, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "/4";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 157);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Temporal Bind [TBv1]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace TemporalBind
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();                   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (File.Exists(openFileDialog1.FileName)) {
+                textBox1.Text = openFileDialog1.FileName;                
+                button3.Enabled = true;
+                textBox5.Text = "0";
+                label1.Text = "/1";
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+            if (File.Exists(openFileDialog2.FileName))
+            {
+                textBox2.Text = openFileDialog2.FileName;                
+                button2.Enabled = true;
+                button4.Enabled = true;
+                textBox5.Text = "0";
+                label1.Text = "/2";
+            }
+            
+        }
+       private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.ShowDialog();
+            if (File.Exists(openFileDialog3.FileName))
+            {
+                textBox3.Text = openFileDialog3.FileName;
+                button5.Enabled = true;
+                textBox5.Text = "0";
+                label1.Text = "/3";
+            }
+        }
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog4.ShowDialog();
+            if (File.Exists(openFileDialog3.FileName))
+            {
+                textBox4.Text = openFileDialog4.FileName;                
+                textBox5.Text = "0";
+                label1.Text = "/4";
+            }
+            
+        }
+
+        void generator() {
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Visible = false;
+            excelApp.Workbooks.Add();
+            var ws = (Microsoft.Office.Interop.Excel.Worksheet)excelApp.ActiveSheet;
+            ws.EnableSelection = Microsoft.Office.Interop.Excel.XlEnableSelection.xlNoSelection;
+            var file1str = System.IO.File.ReadAllText(openFileDialog1.FileName);
+            file1str = file1str.Replace("\u0000", "").Replace("\u0009", "");
+            var row = 2;
+            var timestamp = 1;
+            var index = 2;
+            var content = 3;
+            ws.Columns[1].ColumnWidth = 23;
+            ws.Columns[2].ColumnWidth = 8;
+            /*
+            ws.Cells[1, 1] = "Timestamp";
+            ws.Cells[1, 2] = "Index";
+            ws.Cells[1, content] = openFileDialog1.SafeFileName;
+            */
+            textBox5.Text = "1";
+            progressBar1.Maximum = file1str.Split('\n').Length+1;
+            progressBar1.Minimum = 0;
+            foreach (var text in file1str.Split('\n'))
+            {
+                var splitText = text.Split(' ');
+                if (splitText.Length > 0)
+                {
+                    ws.Cells[row, timestamp] = splitText[0]; // Make sure it is timestamp
+                    ws.Cells[row, index] = string.Format(String.Format("{0:D10}", row));
+                    if (splitText.Length > 1)
+                    {
+                        splitText[0] = "";
+                        string combinedString = "";
+                        foreach (var tx in splitText)
+                        {
+                            combinedString += " " + tx;
+                        }
+                        ws.Cells[row, content] = combinedString.ToString();
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                progressBar1.Value = row;
+                row++;
+            }
+            ws.Columns[content].ColumnWidth = 80;
+            content++;
+
+            if (textBox2.Text != "" && textBox2.Text != textBox1.Text)
+            {
+                textBox5.Text = "2";
+                file1str = System.IO.File.ReadAllText(textBox2.Text);
+                file1str = file1str.Replace("\u0000", "").Replace("\u0009", "");
+                //ws.Cells[1, content] = openFileDialog2.SafeFileName;
+                progressBar1.Maximum = file1str.Split('\n').Length + row;
+                progressBar1.Minimum = row;
+                foreach (var text in file1str.Split('\n'))
+                {
+                    progressBar1.Value = row; ;
+                    var splitText = text.Split(' ');
+                    if (splitText.Length > 0)
+                    {
+                        ws.Cells[row, timestamp] = splitText[0]; // Make sure it is timestamp
+                        ws.Cells[row, index] = string.Format(String.Format("{0:D10}", row));
+                        if (splitText.Length > 1)
+                        {
+                            splitText[0] = "";
+                            string combinedString = "";
+                            foreach (var tx in splitText)
+                            {
+                                combinedString += " " + tx;
+                            }
+                            ws.Cells[row, content] = combinedString.ToString();
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    row++;
+                }
+                ws.Columns[content].ColumnWidth = 80;
+                content++;
+            }
+
+            if (File.Exists(textBox3.Text)) {
+                if (textBox3.Text != "" && textBox2.Text != textBox3.Text && textBox1.Text != textBox3.Text)                    
+                {
+                    file1str = System.IO.File.ReadAllText(textBox3.Text);
+                    file1str = file1str.Replace("\u0000", "").Replace("\u0009", "");
+                    //ws.Cells[1, content] = openFileDialog3.SafeFileName;
+                    progressBar1.Maximum = file1str.Split('\n').Length + row;
+                    progressBar1.Minimum = row;
+                    foreach (var text in file1str.Split('\n'))
+                    {
+                        progressBar1.Value = row;
+                        var splitText = text.Split(' ');
+                        if (splitText.Length > 0)
+                        {
+                            ws.Cells[row, timestamp] = splitText[0]; // Make sure it is timestamp
+                            ws.Cells[row, index] = string.Format(String.Format("{0:D10}", row));
+                            if (splitText.Length > 1)
+                            {
+                                splitText[0] = "";
+                                string combinedString = "";
+                                foreach (var tx in splitText)
+                                {
+                                    combinedString += " " + tx;
+                                }
+                                ws.Cells[row, content] = combinedString.ToString();
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        row++;
+                    }
+                }
+                ws.Columns[content].ColumnWidth = 80;
+                content++;
+            }
+
+            if (File.Exists(textBox4.Text)) {
+                if (textBox4.Text != "" && textBox3.Text != textBox4.Text && textBox2.Text != textBox4.Text && textBox1.Text != textBox4.Text)
+                {
+                    textBox5.Text = "4";
+                    file1str = System.IO.File.ReadAllText(textBox4.Text);
+                    file1str = file1str.Replace("\u0000", "").Replace("\u0009", "");
+                    //ws.Cells[1, content] = openFileDialog4.SafeFileName;
+                    progressBar1.Maximum = file1str.Split('\n').Length + row;
+                    progressBar1.Minimum = row;
+                    foreach (var text in file1str.Split('\n'))
+                    {
+                        progressBar1.Value = row;
+                        var splitText = text.Split(' ');
+                        if (splitText.Length > 0)
+                        {
+                            ws.Cells[row, timestamp] = splitText[0]; // Make sure it is timestamp
+                            ws.Cells[row, index] = string.Format(String.Format("{0:D10}", row));
+                            if (splitText.Length > 1)
+                            {
+                                splitText[0] = "";
+                                string combinedString = "";
+                                foreach (var tx in splitText)
+                                {
+                                    combinedString += " " + tx;
+                                }
+                                ws.Cells[row, content] = combinedString.ToString();
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        row++;
+                    }
+                }
+            }
+
+            ws.Columns[content].ColumnWidth = 80;
+            dynamic allData = ws.UsedRange;
+            allData.Sort(allData.Columns[1], Microsoft.Office.Interop.Excel.XlSortOrder.xlAscending);
+            //ws.Sort(ws.Columns[1], Microsoft.Office.Interop.Excel.XlSortOrder.xlAscending);
+
+            var k = ws.Range[ws.Cells[1, 1], ws.Cells[row, content]];
+            MessageBox.Show("Temporal Merge Completed.");
+            progressBar1.Value = progressBar1.Minimum;            
+            excelApp.Visible = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            generator();
+        }
+
+
+    }
+}
